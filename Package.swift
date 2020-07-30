26654d183553cf9c5cea022f3a2c3c2296358b74
Steve Trewick <steve.trewick@icloud.com>
2020-07-30 13:35:24 +0100

    Add dep

--- Package.swift
+++ Package.swift
@@ -12,15 +12,16 @@ let package = Package(
             targets: ["SourceKitHipster"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // this is just a modmao tha exposes the sourcekitd headers so we can the
+        // appropriate types imported.
+        .package(url: "https://github.com/SteveTrewick/CSourceKitD.git", from: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SourceKitHipster",
-            dependencies: []),
+            dependencies: ["CSourceKitD"]),
         .testTarget(
             name: "SourceKitHipsterTests",
             dependencies: ["SourceKitHipster"]),
